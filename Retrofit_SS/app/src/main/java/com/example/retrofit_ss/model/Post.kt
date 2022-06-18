470cbc593579e3bfbe7dbf5a663b6c0f7ad40031
Lawal Hayde <81729609+Haydelawal@users.noreply.github.com>
2022-06-18 01:59:18 +0100

    Delete Retrofit_SS directory

--- Retrofit_SS/app/src/main/java/com/example/retrofit_ss/model/Post.kt
+++
@@ -1,9 +0,0 @@
-package com.example.retrofit_ss.model
-
-data class Post (
-
-    val userId: Int,
-    val id: Int,
-    val title: String,
-    val body: String
-        )
--- Retrofit_SS/app/src/main/java/com/example/retrofit_ss/utils/Constants.kt
+++
@@ -1,9 +0,0 @@
-package com.example.retrofit_ss.utils
-
-class Constants {
-
-    companion object {
-        const val BASE_URL = "https://jsonplaceholder.typicode.com"
-    }
-
-}
